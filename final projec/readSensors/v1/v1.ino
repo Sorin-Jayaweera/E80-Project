6aded60fac069a32258a8e243722305bbfa4b0e3
WunderBear <sorin@jayaweera.com>
2025-04-02 18:07:03 -0700

    updated readsensors

--- final projec/readSensors/v1/v1.ino
+++ final projec/readSensors/v1/v1.ino
@@ -3,7 +3,7 @@
 #define pressurePin 14 //A0
 #define turbidityPin 15 // A1
 #define phPin 16 // A2
-#define temperaturePin 17
+#define temperaturePin 17 
 #define salinityPin 18
 
 
@@ -22,9 +22,9 @@ void setup() {
 
 
   gravityTds.setPin(salinityPin);
-    gravityTds.setAref(3.3);  //reference voltage on ADC, default 5.0V on Arduino UNO
-    gravityTds.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
-    gravityTds.begin();
+  gravityTds.setAref(3.3);  //reference voltage on ADC, default 5.0V on Arduino UNO
+  gravityTds.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
+  gravityTds.begin();
 
 }
 
@@ -32,27 +32,36 @@ void loop() {
   // put your main code here, to run repeatedly:
   double turbidityVoltage = analogRead(turbidityPin) * 3.3/1023;
   double phVoltage = analogRead(turbidityPin) * 3.3/1023;
-  double pressureVoltage = analogRead(turbidityPin) * 3.3/1023;
+  double depthVoltage = analogRead(turbidityPin) * 3.3/1023;
   double temperatureVoltage = analogRead(temperaturePin) * 3.3/1023;
-  //double tdsVoltage = analogRead(temperaturePin) * 3.3/1023;
+  double tdsVoltage = analogRead(temperaturePin) * 3.3/1023;
 
-  double depth = -94.1 * pressureVoltage + 310; // m
+  double depth = -94.1 * depthVoltage + 310; // m
   double turbidity = -471 * turbidityVoltage + 1498; // NTU ; right now this is linear, when the data shows a skew above and below systematically
   double ph = phVoltage * -2.1429 + 14.5; // PH
   double temperature = temperatureVoltage * -0.0924+3.05;
-  //double tds = 2.02 *10**(-0.3)* (2.71828182835904523)**(2.08)*tdsVoltage;
+  double tds = 2.02 *pow(10,-3) * pow(2.71828,2.08)*tdsVoltage;//TODO: exp should be 2.08
+
 
   gravityTds.setTemperature(temperature);  // set the temperature and execute temperature compensation
   gravityTds.update();  //sample and calculate 
-  tdsValue = gravityTds.getTdsValue();  // then get the value
+  double tdsValue = gravityTds.getTdsValue();  // then get the value
   
   Serial.print("Depth: "); Serial.print(depth);
-  Serial.print(" Turbidity: "); Serial.print(turbidity);
-  Serial.print(" PH: "); Serial.print(ph);
-  //Serial.print("TDS: "); Serial.print(tds);
-  Serial.print(" TDS")
-  Serial.print(tdsValue,0);
-  Serial.println("ppm");
+  Serial.print("  Turbidity: "); Serial.print(turbidity);
+  Serial.print("  PH: "); Serial.print(ph);
+  Serial.print("  Temperature: "); Serial.print(temperature);
+  Serial.print("  TDS(ours): "); Serial.print(tds);
+  Serial.print("  TDS(lib): "); Serial.print(tdsValue);
+  Serial.print("  TDS: ");   Serial.print(tdsValue,0);  Serial.println("ppm");
+
+  Serial.print("Depth V: "); Serial.print(depthVoltage);
+  Serial.print("  Turbidity V: "); Serial.print(depthVoltage);
+  Serial.print("  PH V: "); Serial.print(depthVoltage);
+  Serial.print("  Temperature V: "); Serial.print(temperatureVoltage);
+  Serial.print("  TDS V: "); Serial.println(tdsVoltage);
+  Serial.println("\n \n \n");
+  delay(2000);
 
 
   
