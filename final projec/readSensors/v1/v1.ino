cbddb7b8b4b1e855837e5806801753dc6a237330
WunderBear <sorin@jayaweera.com>
2025-04-02 17:02:55 -0700

    updated read sensors, added tds library

--- final projec/readSensors/v1/v1.ino
+++ final projec/readSensors/v1/v1.ino
@@ -1,6 +1,14 @@
+#include "GravityTDS.h"
+
 #define pressurePin 14 //A0
 #define turbidityPin 15 // A1
 #define phPin 16 // A2
+#define temperaturePin 17
+#define salinityPin 18
+
+
+GravityTDS gravityTds;
+
 void setup() {
   // put your setup code here, to run once:
   
@@ -9,6 +17,15 @@ void setup() {
   pinMode(pressurePin,INPUT);
   pinMode(turbidityPin,INPUT);
   pinMode(phPin,INPUT);
+  pinMode(temperaturePin,INPUT);
+  //pinMode(salinityPin,INPUT);
+
+
+  gravityTds.setPin(salinityPin);
+    gravityTds.setAref(3.3);  //reference voltage on ADC, default 5.0V on Arduino UNO
+    gravityTds.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
+    gravityTds.begin();
+
 }
 
 void loop() {
@@ -16,14 +33,26 @@ void loop() {
   double turbidityVoltage = analogRead(turbidityPin) * 3.3/1023;
   double phVoltage = analogRead(turbidityPin) * 3.3/1023;
   double pressureVoltage = analogRead(turbidityPin) * 3.3/1023;
+  double temperatureVoltage = analogRead(temperaturePin) * 3.3/1023;
+  //double tdsVoltage = analogRead(temperaturePin) * 3.3/1023;
 
   double depth = -94.1 * pressureVoltage + 310; // m
   double turbidity = -471 * turbidityVoltage + 1498; // NTU ; right now this is linear, when the data shows a skew above and below systematically
   double ph = phVoltage * -2.1429 + 14.5; // PH
+  double temperature = temperatureVoltage * -0.0924+3.05;
+  //double tds = 2.02 *10**(-0.3)* (2.71828182835904523)**(2.08)*tdsVoltage;
 
+  gravityTds.setTemperature(temperature);  // set the temperature and execute temperature compensation
+  gravityTds.update();  //sample and calculate 
+  tdsValue = gravityTds.getTdsValue();  // then get the value
+  
   Serial.print("Depth: "); Serial.print(depth);
-  Serial.print("Turbidity: "); Serial.print(turbidity);
-  Serial.print("PH: "); Serial.print(ph);
+  Serial.print(" Turbidity: "); Serial.print(turbidity);
+  Serial.print(" PH: "); Serial.print(ph);
+  //Serial.print("TDS: "); Serial.print(tds);
+  Serial.print(" TDS")
+  Serial.print(tdsValue,0);
+  Serial.println("ppm");
 
 
   
